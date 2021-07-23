6ce255c5a96a664eaa3f5b8888b49d255cb00e6d
Luca <mastrantonio.luca.91@icloud.com>
2021-07-23 15:55:42 +0200

    Palindrome - request word

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,3 +1,8 @@
-// Palidroma
+// Palindroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// CHIEDO ALL'UTENTE UNA PAROLA
+var parola_utente = prompt('inserisci una parola');
+
+// CERO UNA FUNZIONE PER CAPIRE SE LA PAROLA INSERITA E' UNA PAROLA PALINDROMA O NO
