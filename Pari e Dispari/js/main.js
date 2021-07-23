9d2c4922427bbbbd6f0ca566b0e2af65a0c96439
Luca <mastrantonio.luca.91@icloud.com>
2021-07-23 15:48:37 +0200

    Pari e Dispari complete

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -4,3 +4,33 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+//  CHIEDO ALL'UTENTE PARI O DISPARI E DI SCEGLIERE UN NUMERO
+var scelta_utente = prompt('Scegli pari o dispari');
+var numero_utente = parseInt(prompt('inserisci un numero da 1 a 10'));
+console.log('utente ha scelto' + ' ' + scelta_utente + ' e il numero' + ' ' + + numero_utente);
+// GENERO NUMERO RANDOM PER IL COMPUTER
+function numeroRnd (min, max) {
+    var numero_pc = Math.floor(Math.random() * (max - min)) + min;
+    return numero_pc;
+}
+
+var numero_pc = numeroRnd(9, 10);
+console.log('il numero random del pc' + ' ' + numero_pc);
+
+// SOMMA DEI DUE NUMERI
+var somma = numero_utente + numero_pc;
+console.log(somma + ' ' + 'questa è la somma dei due numeri');
+
+// STABILIAMO SE LA SOMMA DEI DUE NUMERI è PARI O DISPARI E VEDIAMO CHI HA VINTO
+function vittoria (num) {
+    if (num % 2 == 0) {
+        return 'Pari vince';
+    }else {
+        return 'dispari vince';
+    }
+    
+}
+
+var risultato = vittoria(somma);
+console.log(risultato);
